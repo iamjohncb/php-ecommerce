e85c3afd26f51e2d8cae261643c140ac9c14ae1c
John <cebotari@riseup.net>
2023-05-03 15:35:50 +0300

    add routes for product management

--- app/routing/routes.php
+++ app/routing/routes.php
@@ -7,3 +7,9 @@ $router->map('GET','/', 'App\controllers\IndexController@show','home');
 //for admin routes
 $router->map('GET','/admin', 'App\controllers\admin\DashboardController@show','admin_dashboard');
 $router->map('POST','/admin', 'App\controllers\admin\DashboardController@get','admin_form');
+
+//product management
+$router->map('GET','/admin/product/categories',
+    'App\controllers\admin\ProductCategoryController@show','product_category');
+$router->map('POST','/admin/product/categories',
+    'App\controllers\admin\ProductCategoryController@store','create_product_category');
