7fc086f9c7741880a2d5048eb5d80b2c095f22e5
John <cebotari@riseup.net>
2023-05-04 12:50:06 +0300

    creating the edit category form

--- resources/views/admin/products/categories.blade.php
+++ resources/views/admin/products/categories.blade.php
@@ -43,8 +43,26 @@
                                 <td>{{$category['slug']}}</td>
                                 <td>{{$category['added']}}</td>
                                 <td width="100" class="text-right">
-                                    <a href="#"><i class="fa fa-edit"></i></a>
+                                    <a data-open="item-{{$category['id']}}"><i class="fa fa-edit"></i></a>
                                     <a href="#"><i class="fa fa-times"></i></a>
+
+                                    <!-- Edit Category Modal -->
+                                    <div class="reveal" id="item-{{$category['id']}}"
+                                         data-reveal data-close-on-click="false" data-close-on-esc="false">
+                                        <h2>Edit Category</h2>
+                                        <form>
+                                            <div class="input-group">
+                                                <input type="text" name="name" value="{{$category['name']}}">
+                                                <input type="hidden" name="token" value="{{\App\classes\CSRFToken::_token()}}">
+                                                <div>
+                                                    <input type="submit" class="button update-category" id="{{$category['id']}}" value="Update">
+                                                </div>
+                                            </div>
+                                        </form>
+                                        <button class="close-button" data-close aria-label="Close modal" type="button">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
                                 </td>
                             </tr>
                         @endforeach
