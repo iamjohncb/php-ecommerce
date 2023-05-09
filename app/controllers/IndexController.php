66d96a7bc3c4e0b955384d1f658d5099ae3611cf
John <cebotari@riseup.net>
2023-05-09 12:37:49 +0300

    setup front store layout

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -6,29 +6,9 @@ use App\classes\Mail;
 
 class IndexController extends BaseController
 {
-
-
-    public function show(){
-
-        //echo "Inside Homepage from controller class";
-
-        $mail = new Mail();
-        $data =[
-          'to' => 'cebotari.john@gmail.com',
-          'subject' => 'Welcome to acme store',
-          'view' => 'welcome',
-          'name' => 'John Doe',
-          'body' => "Testing email template"
-        ];
-
-        if($mail->send($data)){
-            echo "Email send successfully";
-        } else{
-            echo "Email sending failure";
-        }
-
+    public function show()
+    {
+        return view('home');
     }
-
-
 
 }
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.base')
+
+@section('body')
+
+    <!--Navigation -->
+    
+    <!--Site Wrapper -->
+    <div class="site_wrapper">
+        @yield('content')
+    </div>
+    @yield('footer')
+@stop
